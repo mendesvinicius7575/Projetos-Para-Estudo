38dd66d234a659bf39990dbb1e754206b067f819
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-03 08:51:38 -0300

    Tabuada
    Falta inserir quebra de linha nao consegui

--- Tabuada-Guanabara/javas.js
+++ Tabuada-Guanabara/javas.js
@@ -7,15 +7,15 @@ let res = document.querySelector('#res')
 ir.addEventListener('click', function(e){
 
     let num = Number(n.value)
-    let text = '';
+    let text = ''
     let textf = ''
     let numf;
 
     for(let i = 0; i < 10; i++){
         
         numf = num * i
-        text =  num + "x" + i + "=" + numf 
-        textf = textf + text + "<br>"
+        text =  `${num} x ${i} = ${numf}`
+        textf = textf + text 
 
     
     }
